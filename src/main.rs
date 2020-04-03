d044c9c77ab44d171e51b0a269b8c6e4d6e0ef04
Albrecht <14820950+weichweich@users.noreply.github.com>
2020-04-03 15:47:27 +0200

    feat: add options

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
 #![warn(clippy::all)]
+#![deny(unsafe_code)]
 
 use regex::Regex;
-use std::path::Path;
+use std::{error::Error, path::Path};
+use structopt::StructOpt;
 
 mod manipulator;
 mod read_file;
 mod scan;
 
-fn main() {
+const NAME: &str = env!("CARGO_PKG_NAME");
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = NAME)]
+pub struct Opt {
+    #[structopt(
+        short,
+        long,
+        parse(from_occurrences),
+        help = "Sets the level of verbosity."
+    )]
+    verbose: u8,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let opt = Opt::from_args();
+
+    // Configure logger
+    stderrlog::new().verbosity(opt.verbose as usize).init()?;
+
     println!("Hello, world!");
 
     let re_file = Regex::new(r".*\.txt").unwrap();
@@ -18,4 +39,6 @@ fn main() {
     let scan = scan::RegexFilter::new(re_line, Box::new(changer));
 
     read_file::find_files(&dir, re_file, Box::new(scan));
+
+    Ok(())
 }
